cef5c57fddd38ebb64b243691d15852c625fa77d
davidenoma <davidenoma@gmail.com>
2020-11-30 19:59:45 +0100

    addition of OOP

---
+++ check.py
@@ -0,0 +1,3 @@
+import ninth_class_assignment
+book = ninth_class_assignment.Book("Think and Grow Rich","Napoleon","McGraw")
+print(book)
---
+++ ninth_class_assignment.py
@@ -0,0 +1,9 @@
+class Book:
+    def __init__(self, title,author, publisher):
+        self.__title=title
+        self.__author=author
+        self.__publisher=publisher
+    
+def __str__(self):
+        return 'The title of the book is' + self.__title +
+    'Author' + str(self.__author) + 'Publisher' + str(self.__publisher)
